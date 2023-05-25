64e81a7ce84ba212e627a6416097c2ee0619b214
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-05-24 22:19:11 -0300

    Criação da página base para consumir a API

--- alunosapifront/src/App.js
+++ alunosapifront/src/App.js
@@ -1,11 +1,59 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
+
 function App() {
+  const baseUrl = "https://localhost:7195/Alunos";
+  const [data, setData] = useState([]);
+
+  const pedidoGet = async() => {
+    await axios.get(baseUrl)
+    .then(response => {
+      setData(response.data);
+    }).catch(error => {
+      console.log(error);
+    })
+  }
+
+  useEffect(() => {
+    pedidoGet();
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        
+      <br/>
+      <h3>Cadastro de Alunos</h3>
+      <header>
+        <button className="btn btn-success">Incluir Novo Aluno</button>
       </header>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Idade</th>
+            <th>Operação</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(aluno => (
+            <tr key = {aluno.id}>
+              <td>{aluno.id}</td>
+              <td>{aluno.nome}</td>
+              <td>{aluno.email}</td>
+              <td>{aluno.idade}</td>
+              <td>
+                <button className="btn btn-primary">Editar</button> {" "}
+                <button className="btn btn-danger">Excluir</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
